786ed2b4abd1783d9f76cc7af36b2ee7d2ba14e2
borossandor27 <boros.sandor@petrik.hu>
2019-11-15 19:20:58 +0100

    első óra

---
+++ Haromszogek/DHaromszog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haromszogek
+{
+    class DHaromszog
+    {
+        double aOldal;
+        double bOldal;
+        double cOldal;
+        public double a ;
+        public double b ;
+        public double c ;
+        bool ellDerekszogu;
+        bool ellMegszerkesztheto;
+        bool ellNovekvosorrend;
+        public double Kerulet { get => a + b + c; }
+        public int SorSzama { get => sorSzama; set => sorSzama = value; }
+        public double Terulet { get => (a * b) / 2; set => terulet = value; }
+        public DHaromszog(string sor, int sorSzama)
+            {
+            string[] oldalak = sor.Split(' ');
+            aOldal = double.Parse(oldalak[0]);
+            bOldal = double.Parse(oldalak[1]);
+            cOldal = double.Parse(oldalak[2]);
+            if (aOldal<=bOldal && bOldal<=cOldal)
+            {
+                ellNovekvosorrend = true;
+            }
+            else
+            {
+                ellNovekvosorrend = false;
+            }
+            ellMegszerkesztheto = (aOldal + bOldal > cOldal) ? true : false;
+            ellDerekszogu = (aOldal * aOldal + bOldal * bOldal == cOldal * cOldal) ? true : false;
+        }
+        private double terulet;
+        private int sorSzama;
+    }
+}
---
+++ Haromszogek/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Haromszogek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Adatok betöltése";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(502, 123);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hibák a kiválasztott állományban";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(13, 214);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(265, 134);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Derékszögű háromszögek";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(354, 223);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kiválasztott derékszögű háromszög adatai";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Derékszögű háromszögek";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
